380ec8b1d4a54b107beea61dfe22c10e3ced96c2
Aryan Patel <bontaromiii@gmail.com>
2026-06-14 14:41:40 +0530

    fixed some home tiles, added a button for back in pausemenu

--- scripts/pausemenu.gd
+++ scripts/pausemenu.gd
@@ -14,3 +14,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	pass
+	
