cd82c8b87a56fb46ca1899c7d5c286d0f4776079
Aryan Patel <bontaromiii@gmail.com>
2026-06-21 20:28:45 +0530

    removed button texture instead used that texture on pointlight node to make popping animation when hovered or pressed buttons in main menu scene

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -1,16 +1,18 @@
 extends Control
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+@onready var point_light_2d: PointLight2D = $PointLight2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+@onready var mainmenubutton: Sprite2D = $Mainmenubutton
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
 
 
 func _on_mocondorect_mouse_entered() -> void:
@@ -19,3 +21,35 @@ func _on_mocondorect_mouse_entered() -> void:
 
 func _on_mocondorect_pressed() -> void:
 	animation_player.play("moconcorectpressed")
+
+
+func _on_button_mouse_entered() -> void:
+	point_light_2d.visible = true
+	point_light_2d.energy = 0.5
+	
+
+
+func _on_button_pressed() -> void:
+	
+	pass
+	
+
+
+func _on_button_mouse_exited() -> void:
+	
+	point_light_2d.energy = 0.5
+	point_light_2d.visible = false
+
+
+func _on_button_button_down() -> void:
+	point_light_2d.texture_scale = 1.05
+	point_light_2d.visible = true
+	point_light_2d.energy = 1
+
+
+func _on_button_button_up() -> void:
+	point_light_2d.visible = true
+	point_light_2d.texture_scale = 1
+	point_light_2d.energy = 0
+
+	
