9f30cd4bd15fde88c5d33024023d522453f59725
Aryan Patel <bontaromiii@gmail.com>
2026-06-24 14:48:08 +0530

    categorised workshop options by visible property, according to when realed option clicked like fruits clicked and apple,banana,mango etc option are visible, assigned option buttons a group and enabled toggle mod eso only one button clicked at a time

--- scripts/workshop.gd
+++ scripts/workshop.gd
@@ -3,26 +3,30 @@ extends Control
 @onready var workshopselectedui: TextureRect = $Control/Workshopselectedui
 
 var selected = null
+@onready var mocondo_butterfly: Sprite2D = $MocondoButterfly
+@onready var label: Label = $Label
+
 
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
 	selected = $TextureRect2/ScrollContainer2/MarginContainer/VBoxContainer/Button2
-
+	texture_rect_2.visible = false
+	texture_rect_3.visible = false
+	texture_rect_4.visible = false
+	texture_rect_5.visible = false
+	texture_rect_6.visible = false
+	texture_rect_7.visible = false
+	texture_rect_8.visible = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if selected != null:
 		workshopselectedui.global_position.y = selected.global_position.y - 50
+	elif selected:
+		mocondo_butterfly.visible = false
+		label.visible = false
 
-
-func _on_button_2_pressed() -> void:
-	selected = $TextureRect2/ScrollContainer2/MarginContainer/VBoxContainer/Button2
-
-@onready var workshopselectedui_2: Sprite2D = $Workshopselectedui2
-
-func _on_button_3_pressed() -> void:
-	selected = $TextureRect2/ScrollContainer2/MarginContainer/VBoxContainer/Button3
 
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -41,11 +45,52 @@ func _on_button_pressed() -> void:
 	invenrory.visible = true
 	
 	
+@onready var texture_rect_2: TextureRect = $TextureRect2/ScrollContainer/MarginContainer/HBoxContainer/TextureRect2
+@onready var texture_rect_3: TextureRect = $TextureRect2/ScrollContainer/MarginContainer/HBoxContainer/TextureRect3
+@onready var texture_rect_4: TextureRect = $TextureRect2/ScrollContainer/MarginContainer/HBoxContainer/TextureRect4
+@onready var texture_rect_5: TextureRect = $TextureRect2/ScrollContainer/MarginContainer/HBoxContainer/TextureRect5
+@onready var texture_rect_6: TextureRect = $TextureRect2/ScrollContainer/MarginContainer/HBoxContainer/TextureRect6
+@onready var texture_rect_7: TextureRect = $TextureRect2/ScrollContainer/MarginContainer/HBoxContainer/TextureRect7
+@onready var texture_rect_8: TextureRect = $TextureRect2/ScrollContainer/MarginContainer/HBoxContainer/TextureRect8
 
+func _on_button_2_pressed() -> void:
+	selected = $TextureRect2/ScrollContainer2/MarginContainer/VBoxContainer/Button2
+	
+	texture_rect_2.visible = false
+	texture_rect_3.visible = true
+	texture_rect_4.visible = true
+	texture_rect_5.visible = true
+	texture_rect_6.visible = true
+	texture_rect_7.visible = false
+	texture_rect_8.visible = false
+@onready var workshopselectedui_2: Sprite2D = $Workshopselectedui2
+
+func _on_button_3_pressed() -> void:
+	selected = $TextureRect2/ScrollContainer2/MarginContainer/VBoxContainer/Button3
+	texture_rect_2.visible = true
+	texture_rect_3.visible = false
+	texture_rect_4.visible = false
+	texture_rect_5.visible = false
+	texture_rect_6.visible = false
+	texture_rect_7.visible = false
+	texture_rect_8.visible = false
 
 func _on_button_4_pressed() -> void:
 	selected = $TextureRect2/ScrollContainer2/MarginContainer/VBoxContainer/Button4
-
+	texture_rect_2.visible = false
+	texture_rect_3.visible = false
+	texture_rect_4.visible = false
+	texture_rect_5.visible = false
+	texture_rect_6.visible = false
+	texture_rect_7.visible = false
+	texture_rect_8.visible = false
 
 func _on_button_5_pressed() -> void:
 	selected = $TextureRect2/ScrollContainer2/MarginContainer/VBoxContainer/Button5
+	texture_rect_2.visible = false
+	texture_rect_3.visible = false
+	texture_rect_4.visible = false
+	texture_rect_5.visible = false
+	texture_rect_6.visible = false
+	texture_rect_7.visible = false
+	texture_rect_8.visible = false
