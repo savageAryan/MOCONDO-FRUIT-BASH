134bfa802d38c868f0988e00194cd626f91a8d21
Aryan Patel <bontaromiii@gmail.com>
2026-06-29 19:18:02 +0530

    actully i wasnt able to switch between hotbar slots, i realissed that tab in godot 4.7(recently updated) is a special key, and i also wanted mouse selction to work with scroll wheel,so i did use it but it caused them to skip a skot everytime cause i realised that mouse produced 2 to 3 inputs at very very pinch time so i added a timer and a variable that stops mouse scrool input for that timer

--- scripts/hotbar.gd
+++ scripts/hotbar.gd
@@ -3,6 +3,7 @@ extends TextureRect
 @onready var slots: Array = $Container.get_children()
 @onready var selector: Sprite2D = $selector
 var currently_selected: int = 0
+var scrool = true
 func update() -> void:
 	for i in range(slots.size()):
 		var inventory_slot: InventorySlot = inventory.slots[i]
@@ -10,13 +11,25 @@ func update() -> void:
 func move_selector() -> void:
 	currently_selected = (currently_selected +1)% slots.size()
 	selector.global_position = slots[currently_selected].global_position
+func move_selectorback() -> void:
+	currently_selected = (currently_selected -1)% slots.size()
+	selector.global_position = slots[currently_selected].global_position
 
 func _unhandled_input(event) -> void:
 	if event.is_action_pressed("use"):
 		inventory.use_Item_at_Index(currently_selected)
-	if Input.is_action_just_pressed("inventory_selection"):
-		print("selector")
-		move_selector()
+	if event is InputEventMouseButton and scrool == true:
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			move_selector()
+			scrool = false
+			await get_tree().create_timer(0.05).timeout
+			scrool = true
+	if event is InputEventMouseButton and scrool == true:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			move_selectorback()
+			scrool = false
+			await get_tree().create_timer(0.05).timeout
+			scrool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
