adeb5f44ef68d287c5da57f0fa46f5e15ebb70da
Aryan Patel <bontaromiii@gmail.com>
2026-07-10 16:05:32 +0530

    added point light with button texture and using signals mouse in and out if mouse is inpoint light shows if no point light dont show,added new main menu button with same tween animation and respawn and also the point light mechanism,the main menu point light is not showing even though its the same will fix

--- scripts/death_scene.gd
+++ scripts/death_scene.gd
@@ -2,6 +2,8 @@ extends Control
 @onready var color_rect: ColorRect = $ColorRect
 @onready var player: Player = $"../../player"
 @onready var label: Label = $Label
+@onready var point_light_2d: PointLight2D = $PointLight2D
+@onready var button: Button = $Button
 func _ready() -> void:
 	visible = false
 
@@ -17,3 +19,37 @@ func _on_player_player_dead() -> void:
 	
 	label.visible = true
 	label_tween.tween_property(label,"modulate",Color("fff9f7e7"),5)
+	await label_tween.finished
+	var button_tween = create_tween()
+	button.visible = true
+	button_2.visible = true
+	button_tween.tween_property(button,"modulate",Color("fff"),3)
+	button_tween.tween_property(button_2,"modulate",Color("fff"),3)
+func _on_button_mouse_entered() -> void:
+	point_light_2d.visible = true
+	point_light_2d.energy = 0.5
+func _on_button_mouse_exited() -> void:
+	point_light_2d.energy = 0.5
+	point_light_2d.visible = false
+
+@onready var button_2: Button = $Button2
+
+func _on_button_pressed() -> void:
+	await get_tree().create_timer(1.2).timeout
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+@onready var point_light_2d_2: PointLight2D = $PointLight2D2
+
+func _on_button_2_mouse_entered() -> void:
+	point_light_2d_2.visible = true
+	point_light_2d_2.energy = 0.5
+
+
+func _on_button_2_mouse_exited() -> void:
+	point_light_2d_2.visible = false
+	point_light_2d_2.energy = 0.5
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().create_timer(1.2).timeout
+	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
