6d6611c606f0d6693389c3794e44ab2f9fb81527
Aryan Patel <bontaromiii@gmail.com>
2026-07-12 10:15:24 +0530

    added capybara.tscn for a capybara mob, literally copied blob roaming() for it just fixed some details, i don't even have all sprite for it in my hand rn will make as i go

---
+++ scripts/capybara.gd
@@ -0,0 +1,78 @@
+class_name capybara extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+var ramdon_target = Vector2.ZERO
+var facing = "front"
+var speed = 40
+var waiting = false
+var stuck_pos = Vector2.ZERO
+var stuck_time = 0.0
+func _physics_process(delta: float) -> void:
+	roaming()
+	if global_position.distance_to(stuck_pos) < 1:
+		stuck_time += delta
+	else: stuck_time = 0
+	stuck_pos = global_position
+	if stuck_time > 5.0:
+		stuck_time = 0
+		random_Targer()
+func _ready() -> void:
+	random_Targer()
+	
+func random_Targer():
+
+	
+	ramdon_target = global_position + Vector2(randf_range(-200,200), randf_range(-200,200))
+	navigation_agent_2d.target_position = ramdon_target
+func roaming():
+	if waiting:
+		play_idel()
+		return
+	
+	if navigation_agent_2d.is_navigation_finished():
+		velocity = Vector2.ZERO
+		play_idel()
+		move_and_slide()
+		return
+	var point_X = navigation_agent_2d.get_next_path_position()
+	
+	var direction = (point_X - global_position).normalized()
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			facing = "side"
+			animated_sprite_2d.play("side walking")
+			animated_sprite_2d.flip_h = true
+		else: 
+			facing = "side"
+			animated_sprite_2d.play("side walking")
+			animated_sprite_2d.flip_h = false
+	else:
+		facing = "front"
+		if direction.y > 0:
+			animated_sprite_2d.play("walking front")
+		else:
+			facing = "back"
+			animated_sprite_2d.play("back walking")
+	velocity = velocity.move_toward(direction * speed, 6)
+	
+	move_and_slide()
+	if velocity.length() < 1:
+		play_idel()
+	if navigation_agent_2d.is_navigation_finished():
+		velocity = Vector2.ZERO
+		play_idel()
+		waiting = true
+		
+		await get_tree().create_timer(3).timeout
+		random_Targer()
+		waiting = false
+
+func play_idel():
+	if velocity.length() < 1:
+		if facing == "front":
+			animated_sprite_2d.play("bara idel")
+		elif facing == "back":
+			animated_sprite_2d.play("bara idel")
+		else:
+			if facing == "side":
+				animated_sprite_2d.play("bara idel")
