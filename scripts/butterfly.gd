edda1b658f176b8f62c363460de42dcafcbd3eec
Aryan Patel <bontaromiii@gmail.com>
2026-08-03 14:12:20 +0530

    added logic for butterfly to wobble, earlier i added wobble myself for animated sprite of butterfly y axis but when i wanted to make it look more natural i asked gpt and it looks bad the wobble is not consistent, i will try myself again

--- scripts/butterfly.gd
+++ scripts/butterfly.gd
@@ -1,26 +1,43 @@
 extends Area2D
-var speed = 20
+var speed = randf_range(40,70)
 enum states{fly,rest}
 var state = states.fly
 var facing = null
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 var target: Vector2
+var fixed_pos = null
+var t:= 0.0
+var wobble_amount := 0
+var wobble_time := 0
+var wobble_strength = 0.3
+var wobble_speed = 8.0
+var wobble_timer = 0.0
 func _ready() -> void:
 	new_target()
 func _physics_process(delta: float) -> void:
+	
 	match state:
 		states.fly:
 			fly(delta)
 		states.rest:
 			pass
 func new_target():
-	var radius = 200
-	target = global_position + Vector2(randf_range(-radius,radius),randf_range(-radius,radius))
+	fixed_pos = global_position
+	var radius = randf_range(50,70)
+	target = fixed_pos + Vector2(randf_range(-radius,radius),randf_range(-radius,radius))
 	
 func fly(delta):
+	if randf() < 0.003:
+		target += Vector2(
+		randf_range(-50,50),randf_range(-50,50))
+	if wobble_timer < 0:
+		wobble_timer = randf_range(0.4,1.3)
+		wobble_amount = randf_range(3,7)
+		wobble_time = randf_range(20,50)
+		wobble_strength = randf_range(0.1, 0.6)
+		wobble_speed = randf_range(4.0, 12.0)
 	var direction = (target - global_position).normalized()
 	var distance = global_position.distance_to(target)
-	global_position = global_position.move_toward(target, speed * delta)
 	if abs(direction.x) > abs(direction.y):
 		if direction.x > 0:
 			facing = "side"
@@ -37,7 +54,13 @@ func fly(delta):
 		else:
 			facing = "front"
 			animated_sprite_2d.play("default")
-	
+	t += delta
+	var dir = (target - global_position).normalized()
+	var perp = Vector2(-dir.y,dir.x)
+	var wobble = sin(t * wobble_time)* wobble_strength + sin(t + 2.3)* 0.15
+	var move_dir = (dir + perp * wobble).normalized()
+	global_position += move_dir * speed * delta
+	#animated_sprite_2d.position += dir + perp * randf_range(-5,5)
 	if distance < 2:
 		state = states.rest
 		rest()
