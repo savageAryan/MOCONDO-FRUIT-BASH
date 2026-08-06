412d2771e7155e51483ee5b9ab64dcb0ea271c65
Aryan Patel <bontaromiii@gmail.com>
2026-08-06 10:42:35 +0530

    worked more on the swarm of butterflies,actually delted the script butterflies in swarm and moved the code to butterflyswarm cause it would be easir to access offsets took help from gpt to get the logic for wobble_time

--- scripts/butterflyswarm.gd
+++ scripts/butterflyswarm.gd
@@ -1,23 +1,32 @@
 extends Node2D
 var target: Vector2
+var offsets = {}
+var wobble_times = {}
 @onready var butterflies: Node2D = $butterflies
 @onready var animated_sprite_2d: AnimatedSprite2D = $butterflies/butterfly/AnimatedSprite2D
 @onready var butterfly: Area2D = $butterfly
 var speed:float = 30
 func _ready() -> void:
+	for butterfly in butterflies.get_children():
+		butterfly.get_node("AnimatedSprite2D").play("fly front")
+		offsets[butterfly] = Vector2(randf_range(20,20),randf_range(20,20))
+		wobble_times[butterfly] = randf() * TAU
 	new_target()
-	animated_sprite_2d.play("fly front")
+func _process(delta: float) -> void:
+	pass
 func _physics_process(delta: float) -> void:
 	swift_fly(delta)
 func new_target():
-	var range = randf_range(70,140)
+	var range = randf_range(120,200)
 	target = Vector2(randf_range(-range,range),randf_range(-range,range))
 func swift_fly(delta):
 	for butterfly in butterflies.get_children():
-		pass
-		
+		wobble_times[butterfly] += delta
+		var wobble = Vector2(sin(wobble_times[butterfly] * 6),cos(wobble_times[butterfly] * 8)* 4)
+		var desired_pos = global_position + offsets[butterfly] + wobble
+		butterfly.global_position = butterfly.global_position.move_toward(desired_pos,40 * delta)
 	var direction = (target - global_position).normalized()
-	var distance = global_position.direction_to(target)
+	var distance = global_position.distance_to(target)
 	global_position = global_position.move_toward(target,speed * delta)
 	if direction.length() > 0:
 		rotation = direction.angle() + deg_to_rad(90)
