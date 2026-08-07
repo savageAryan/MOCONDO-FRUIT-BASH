88878d11252a12b552d937f158548f2f8a207cce
Aryan Patel <bontaromiii@gmail.com>
2026-08-07 18:29:22 +0530

    finally got the butterfly swarm right had been debugging and playing with the values to fine tween now i will add lighting that i would need to create a shader or a custom texture for

--- scripts/butterflyswarm.gd
+++ scripts/butterflyswarm.gd
@@ -18,6 +18,7 @@ var formation_time = randf_range(5,8)
 var pass_player = 0.0
 var time_pass = 0.0
 var player_pass:bool = false
+@onready var flowers: Node2D = $"../flowers"
 var next_player_pass = randf_range(8.8,15.0)
 func _ready() -> void:
 	randomize()
@@ -25,17 +26,17 @@ func _ready() -> void:
 		var sprite = butterfly.get_node("AnimatedSprite2D")
 		sprite.play("fly front")
 		sprite.speed_scale = randf_range(0.7,1.3)
-		offsets[butterfly] = Vector2(randf_range(-35,35),randf_range(-20,20))
+		sprite.scale *= randf_range(0.8,1.6)
+		offsets[butterfly] = Vector2(randf_range(-65,65),randf_range(-60,60))
 		target_offsets[butterfly] = offsets[butterfly]
 		wobble_time[butterfly] = randf() * TAU
 		follow_speed[butterfly] = randf_range(3,5)
-	
 	new_target()
 func _physics_process(delta: float) -> void:
 	swift_fly(delta)
 	butterfly_fly(delta)
 func new_target():
-	target = Vector2(randf_range(-1800,1800),randf_range(-1200,1200))
+	target = flowers.get_children().pick_random().global_position
 func swift_fly(delta):
 	var desired = (target - global_position).normalized()
 	speed = lerp(speed,target_speed,1.5*delta)
@@ -43,20 +44,22 @@ func swift_fly(delta):
 	velocity = velocity.slerp(desired,turn_strngth * delta)
 	velocity = velocity.normalized()
 	var turn_amount = abs(velocity.cross(desired))
-	if pass_player:
-		turn_speed = 95.0
+	turn_speed = 95.0
+	if player_pass:
+		target_speed = 95.0
 	else:
 		target_speed = lerp(90,40,turn_amount)
 	speed = lerp(speed,target_speed,delta * 0.8)
 	global_position += velocity * speed * delta
 	
-	if global_position.distance_to(target) < 70:
+	if global_position.distance_to(target) < 12:
 		if player_pass:
+			player_pass = false
 			return
 		new_target()
 func butterfly_fly(delta):
-	time_pass += delta
 	pass_player += delta
+	time_pass += delta
 	formation_timer += delta
 	for butterfly in butterflies.get_children():
 		wobble_time[butterfly]+=delta
@@ -86,10 +89,12 @@ func butterfly_fly(delta):
 			target_offsets[fly_1] = target_offsets[fly_2]
 			target_offsets[fly_2] = change_pos
 	if pass_player > next_player_pass:
-		player_pass = 0
-		next_player_pass = randf_range(8.0,15.8)
 		
-		across_player()
+		next_player_pass = randf_range(8.0,15.8)
+		var player = get_tree().get_first_node_in_group("player")
+		if global_position.distance_to(player.global_position) > 140:
+			across_player()
+			player_pass = true
 		if time_pass > randf_range(4,8):
 			if randf() < 0.02:
 				target_speed = randf_range(70,95)
@@ -102,10 +107,11 @@ func across_player():
 	if randf() > 0.5:
 		side = -1
 	var start =  player.global_position \
-	+ Vector2(-side * 1200,randf_range(-350,350))
+	+ Vector2(-side * 900,randf_range(-80,70))
 	var finish = player.global_position \
-	+ Vector2(side * 1200,randf_range(-350,350))
+	+ Vector2(side * 900,randf_range(-80,70))
 	target_speed = 95.0
 	global_position = start
+	velocity = (finish - start).normalized()
 	player_pass = true
 	target = finish
